5637a57b57e4059468e350cb4dad983da19991bf
jishnumenona <jishnumenona@gmail.com>
2024-03-17 22:07:42 -0700

    first commit

---
+++ scene_1/config.py
@@ -0,0 +1,13 @@
+from ptk.config import DeviceKeys
+
+DKObj = DeviceKeys()
+
+LIST_OF_DEVICES = [
+    {
+        DKObj.KEY_NAME: "broker",
+        DKObj.KEY_NUMBER: 1,
+        DKObj.KEY_IP: "192.168.1.174",
+        DKObj.KEY_USERNAME: "pi",
+        DKObj.KEY_PASSWORD: "raspberry"
+    }
+]
